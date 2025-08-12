1ec55ef87f071e925e9efae3600973b83e44ddc0
SreejaNallapu <sreejanallapu27@gmail.com>
2025-08-12 11:20:58 +0530

    Add files via upload

---
+++ lab1/Calculatebill.java
@@ -0,0 +1,16 @@
+package lab1;
+import java.util.Scanner;
+public class Calculatebill {
+	public static void main(String[] args) {
+		double bill,result;
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter the bill:");
+		 bill=sc.nextDouble();
+		if(bill>1000) {
+			result=bill-(bill*0.1);
+		}else {
+			result=bill-(bill*0.05);
+		}
+        System.out.println("Total Bill after discount: "+result);
+    }
+}
---
+++ lab1/bufferdemo.java
@@ -0,0 +1,10 @@
+package lab1;
+
+public class bufferdemo {
+	public static void main() {
+		StringBuffer sb1=new StringBuffer("HI");
+		StringBuffer sb2=new StringBuffer("Mere name sreeja hai");
+		System.out.println(sb1);
+		System.out.println(sb2);
+	}
+}
